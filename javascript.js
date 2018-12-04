03803ffa7064e48c7cd2f8312e83a8d690a20fbf
Franco <francobaldassarerodriguez@gmail.com>
2018-12-03 23:18:18 -0300

    comit final

---
+++ javascript.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+    var slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+        setTimeout(showSlides, 2000); // Change image every 2 seconds
+    }
+})
